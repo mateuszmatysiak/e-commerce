736e464923cfd880a7cf8b3b407e4d351c7cfacd
Mateusz Matysiak <mateuszmatysiak96@gmail.com>
2023-04-04 20:45:49 +0200

    Setup Karla font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false,
+  media: false,
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-karla)", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [require("@tailwindcss/aspect-ratio"), require("@tailwindcss/forms")],
 };
