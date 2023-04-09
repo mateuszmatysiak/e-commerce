ffc8755c2f6fbc07eb9b58e6951d29e431ec0208
Mateusz Matysiak <mateuszmatysiak96@gmail.com>
2023-04-09 22:28:49 +0200

    Add color in open state in AccountMenu & ThemeSelect

--- features/Layout/components/AccountMenu.tsx
+++ features/Layout/components/AccountMenu.tsx
@@ -11,49 +11,57 @@ export const AccountMenu = () => {
 
   return (
     <Menu as="div" className="relative inline-block text-left">
-      <Menu.Button className="relative w-10 h-10 rounded-full overflow-hidden border border-slate-800 hover:border-blue-700">
-        <Image
-          src={session?.user?.image || userAvatar}
-          alt="User image"
-          fill={true}
-          loading="lazy"
-          sizes="(max-width: 768px) 100vw,
+      {({ open }) => (
+        <>
+          <Menu.Button
+            className={`relative w-10 h-10 rounded-full overflow-hidden border border-slate-800 hover:border-blue-700 ${
+              open ? "border-blue-700" : ""
+            }`}
+          >
+            <Image
+              src={session?.user?.image || userAvatar}
+              alt="User image"
+              fill={true}
+              loading="lazy"
+              sizes="(max-width: 768px) 100vw,
         (max-width: 1200px) 50vw,
         33vw"
-        />
-      </Menu.Button>
+            />
+          </Menu.Button>
 
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-200 text-black bg-white rounded-md shadow-lg">
-          <Menu.Item>
-            <div className="flex items-center gap-2 py-3 px-4">
-              <UserIcon className="w-5 h-5" aria-hidden="true" />
-              {session?.user?.name}
-            </div>
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <button
-                className={`flex items-center gap-2 py-3 px-4 w-full rounded-b-md ${
-                  active ? "bg-blue-100 text-blue-900" : "text-gray-900"
-                }`}
-                onClick={() => signOut()}
-              >
-                <ArrowLeftOnRectangleIcon className="w-5 h-5" aria-hidden="true" />
-                Wyloguj się
-              </button>
-            )}
-          </Menu.Item>
-        </Menu.Items>
-      </Transition>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-200 text-black bg-white rounded-md shadow-lg">
+              <Menu.Item>
+                <div className="flex items-center gap-2 py-3 px-4">
+                  <UserIcon className="w-5 h-5" aria-hidden="true" />
+                  {session?.user?.name}
+                </div>
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    className={`flex items-center gap-2 py-3 px-4 w-full rounded-b-md ${
+                      active ? "bg-blue-100 text-blue-900" : "text-gray-900"
+                    }`}
+                    onClick={() => signOut()}
+                  >
+                    <ArrowLeftOnRectangleIcon className="w-5 h-5" aria-hidden="true" />
+                    Wyloguj się
+                  </button>
+                )}
+              </Menu.Item>
+            </Menu.Items>
+          </Transition>
+        </>
+      )}
     </Menu>
   );
 };
--- features/Layout/components/ThemeSelect.tsx
+++ features/Layout/components/ThemeSelect.tsx
@@ -14,47 +14,57 @@ export const ThemeSelect = () => {
 
   return (
     <Listbox as="div" value={theme} onChange={setTheme}>
-      <Listbox.Button className="relative rounded-md bg-blue-600 text-white py-2 pl-6 pr-12 text-left hover:bg-blue-700">
-        <span className="block truncate">Motyw</span>
-        <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-          <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-        </span>
-      </Listbox.Button>
-      <Transition
-        as={Fragment}
-        leave="transition ease-in duration-100"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
-      >
-        <Listbox.Options className="absolute -mt-44 max-h-60 overflow-auto rounded-md bg-white py-1 shadow-md">
-          {THEMES.map(({ label, value }) => (
-            <Listbox.Option
-              key={value}
-              className={({ active }) =>
-                `relative cursor-pointer select-none py-2 pl-10 pr-4 ${
-                  active ? "bg-blue-100 text-blue-900" : "text-gray-900"
-                }`
-              }
-              value={value}
-            >
-              {({ selected }) => (
-                <>
-                  <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
-                    {label}
-                  </span>
+      {({ open }) => (
+        <>
+          <Listbox.Button
+            className={`relative rounded-md bg-blue-600 text-white py-2 pl-6 pr-12 text-left hover:bg-blue-700 ${
+              open ? "bg-blue-700" : ""
+            }`}
+          >
+            <span className="block truncate">Motyw</span>
+            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+            </span>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="absolute -mt-44 max-h-60 overflow-auto rounded-md bg-white py-1 shadow-lg">
+              {THEMES.map(({ label, value }) => (
+                <Listbox.Option
+                  key={value}
+                  className={({ active }) =>
+                    `relative cursor-pointer select-none py-2 pl-10 pr-4 ${
+                      active ? "bg-blue-100 text-blue-900" : "text-gray-900"
+                    }`
+                  }
+                  value={value}
+                >
+                  {({ selected }) => (
+                    <>
+                      <span
+                        className={`block truncate ${selected ? "font-medium" : "font-normal"}`}
+                      >
+                        {label}
+                      </span>
 
-                  {selected ? (
-                    <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-blue-600">
-                      <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                      <span className="sr-only">Ikona oznaczająca aktywną opcję</span>
-                    </span>
-                  ) : null}
-                </>
-              )}
-            </Listbox.Option>
-          ))}
-        </Listbox.Options>
-      </Transition>
+                      {selected ? (
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-blue-600">
+                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          <span className="sr-only">Ikona oznaczająca aktywną opcję</span>
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </>
+      )}
     </Listbox>
   );
 };
