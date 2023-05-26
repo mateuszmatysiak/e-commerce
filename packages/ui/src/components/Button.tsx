d2b5f3c76a21f5e68ac555ddb175372aab1d278a
Mateusz Matysiak <mateuszmatysiak96@gmail.com>
2023-05-26 11:40:36 +0200

    Add twMerge in Button

--- packages/ui/src/components/Button.tsx
+++ packages/ui/src/components/Button.tsx
@@ -1,10 +1,8 @@
+import { twMerge } from "tailwind-merge";
+
 interface ButtonProps extends React.ComponentProps<"button"> {}
 
 export const Button = ({ className = "", ...restProperties }: ButtonProps) => {
-  return (
-    <button
-      className={`px-4 py-2 rounded-md text-white bg-blue-600 hover:bg-blue-700 ${className}`}
-      {...restProperties}
-    />
-  );
+  const c = twMerge(`rounded-md bg-blue-600 px-4 py-2 text-white hover:bg-blue-700`, className);
+  return <button className={c} {...restProperties} />;
 };
