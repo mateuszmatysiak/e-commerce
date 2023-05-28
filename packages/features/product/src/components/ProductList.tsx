fbf72f9b854548d2c5ccf792207a5087954095b7
Mateusz Matysiak <mateuszmatysiak96@gmail.com>
2023-05-28 17:18:05 +0200

    Add twMerge to ProductList

--- packages/features/product/src/components/ProductList.tsx
+++ packages/features/product/src/components/ProductList.tsx
@@ -1,13 +1,17 @@
+import { twMerge } from "tailwind-merge";
+
 import { type Product } from "@app/database";
 import { ProductListItem } from "./ProductListItem";
 
 interface ProductListProps {
   products: Product[];
+  className?: string;
 }
 
-export const ProductList = ({ products }: ProductListProps) => {
+export const ProductList = ({ products, className }: ProductListProps) => {
+  const c = twMerge(`grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3`, className);
   return (
-    <ul className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+    <ul className={c}>
       {products.map((product) => {
         return <ProductListItem key={product.id} product={product} />;
       })}
