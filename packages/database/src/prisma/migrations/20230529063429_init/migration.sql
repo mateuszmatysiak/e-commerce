bd5dae5bf250f794c50e990f75b5956de64a843a
Mateusz Matysiak <mateuszmatysiak96@gmail.com>
2023-05-29 10:29:02 +0200

    Update db - schema, seed, scripts

---
+++ packages/database/src/prisma/migrations/20230529063429_init/migration.sql
@@ -0,0 +1,121 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DECIMAL(65,30) NOT NULL,
+    "image" TEXT NOT NULL,
+    "categoryId" TEXT NOT NULL,
+    "sizeId" TEXT NOT NULL,
+    "colorId" TEXT NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Color" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Size" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "provider_account_id" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+    "oauth_token_secret" TEXT,
+    "oauth_token" TEXT,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "session_token" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT,
+    "emailVerified" TIMESTAMP(3),
+    "image" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "VerificationToken" (
+    "id" SERIAL NOT NULL,
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "VerificationToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Color_id_key" ON "Color"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Size_id_key" ON "Size"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_id_key" ON "Category"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_provider_provider_account_id_key" ON "Account"("provider", "provider_account_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_session_token_key" ON "Session"("session_token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_sizeId_fkey" FOREIGN KEY ("sizeId") REFERENCES "Size"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_colorId_fkey" FOREIGN KEY ("colorId") REFERENCES "Color"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- packages/database/src/prisma/seed.ts
+++ packages/database/src/prisma/seed.ts
@@ -1,6 +1,36 @@
 import { prisma } from "./client";
 
 async function main() {
+  await prisma.size.createMany({
+    data: [
+      { id: "xs", name: "XS" },
+      { id: "s", name: "S" },
+      { id: "m", name: "M" },
+      { id: "l", name: "L" },
+      { id: "xl", name: "XL" },
+      { id: "2xl", name: "2XL" },
+    ],
+  });
+
+  await prisma.color.createMany({
+    data: [
+      { id: "czarny", name: "Czarny" },
+      { id: "bialy", name: "Biały" },
+      { id: "szary", name: "Szary" },
+      { id: "czerwony", name: "Czerwony" },
+      { id: "niebieski", name: "Niebieski" },
+      { id: "zielony", name: "Zielony" },
+    ],
+  });
+
+  await prisma.category.createMany({
+    data: [
+      { id: "koszulki", name: "Koszulki" },
+      { id: "bluzy", name: "Bluzy" },
+      { id: "czapki", name: "Czapki" },
+    ],
+  });
+
   await prisma.product.createMany({
     data: [
       {
@@ -8,14 +38,53 @@ async function main() {
         description:
           "Czarna koszula jest uniwersalnym i ponadczasowym elementem, który można łatwo wystylizować na każdą okazję.",
         price: 30,
-        image: "https://images.unsplash.com/photo-1618354691373-d851c5c3a990",
+        image:
+          "https://img.freepik.com/free-photo/short-sleeve-black-t-shirt_1409-2226.jpg?w=1800&t=st=1685345329~exp=1685345929~hmac=ab3de929f45920091dd11aca424bd4b8fd1b7733425ae844d2935d959a5cf9c3",
+        sizeId: "s",
+        colorId: "czarny",
+        categoryId: "koszulki",
       },
       {
-        name: "Kubki",
+        name: "Biała koszulka",
         description:
-          "Kubki to klasyczny i ponadczasowy element, który emanuje elegancją i wszechstronnością.",
-        price: 15.5,
-        image: "https://images.unsplash.com/photo-1605714117967-9fe201ddfe9d",
+          "Biała koszulka idealna na letnie dni. Wykonana z wysokiej jakości bawełny, gwarantuje komfort noszenia i oddychalność nawet podczas gorących dni.",
+        price: 25,
+        image:
+          "https://img.freepik.com/free-photo/white-shirt_144627-34317.jpg?w=1800&t=st=1685345390~exp=1685345990~hmac=4a49119e9a038cfe619ee7e913b28879cae5a71bec480e9f283314c5d3dd7e27",
+        sizeId: "m",
+        colorId: "bialy",
+        categoryId: "koszulki",
+      },
+      {
+        name: "Biała zimowa czapka",
+        description: "Biała zimowa czapka wykonana z miękkiego materiału, idealna na chłodne dni.",
+        price: 20,
+        image:
+          "https://img.freepik.com/free-photo/closeup-shot-white-wool-baby-hat-isolated-white-background_181624-28822.jpg?w=2000&t=st=1685343359~exp=1685343959~hmac=e1b046991520dcc71a79eda5727e8b5be5f12ac07b0812583a593d94140482fe",
+        sizeId: "m",
+        colorId: "bialy",
+        categoryId: "czapki",
+      },
+      {
+        name: "Czerwona zimowa czapka",
+        description: "Czerwona zimowa czapka z pomponem, dodaj koloru do swojej stylizacji.",
+        price: 15,
+        image:
+          "https://img.freepik.com/free-photo/cute-winter-hat_1101-542.jpg?1&w=996&t=st=1685344547~exp=1685345147~hmac=a9f42429ff7f7030c68cadcf8653e1bcb5b8b3f1b2523af1dd75ac6fe354d3b5",
+        sizeId: "m",
+        colorId: "czerwony",
+        categoryId: "czapki",
+      },
+      {
+        name: "Szara bluza z kapturem",
+        description:
+          "Szara bluza z kapturem, wygodna i stylowa. Wykonana z miękkiego materiału, zapewnia przyjemne uczucie na skórze i doskonałe dopasowanie.",
+        price: 35,
+        image:
+          "https://img.freepik.com/free-photo/isolated-grey-hoodie_125540-738.jpg?w=2000&t=st=1685344621~exp=1685345221~hmac=ff412e29649e89611dbfd4494e3caed5bfa6ce499d51da3058324331ca8a02a6",
+        sizeId: "l",
+        colorId: "szary",
+        categoryId: "bluzy",
       },
     ],
   });
